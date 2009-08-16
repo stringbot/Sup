5eecc7926dfb4107ee4be7ed33ec0cf7617de7fa
Joshua Davison <josh@Warszawa.local>
2009-08-16 16:58:41 -0500

    make sup into a standalone class and test it

--- lib/sup.rb
+++ lib/sup.rb
@@ -1,41 +1,23 @@
-require 'rubygems'
-require 'sinatra'
-require 'net/http'
 require 'uri'
+require 'net/http'
 
-get "/" do
-  status_hash = is_finance_running?("web1va", "web2va", "web3va", "dev02")
-  output = style_tag
-  output << "<div id='container'>"
-  output << "<h1>Is Finan<span>&#162;</span>e App Running?</h1>"
-  status_hash.each do |host,value|
-    status = value ? 'up' : 'down'
-    output << "<div class='#{status}'>Finance is #{status} on <span class='host'>#{host}</span>.</div>"
+CONFIG = File.join('..', 'config', 'config.yml')
+
+class Sup
+  def initialize()
+    @config = read_config
   end
-  output << "</div>"
-  output.join
-end
 
-helpers do
-  def is_finance_running?(*servers)
-    status_hash = {}
-    servers.each do |host|
-      Net::HTTP.start("#{host}", 80) do |http|
-        response = http.head('/customers.js?name=22%20Squared')
-        status_hash[host] = response.kind_of?(Net::HTTPSuccess)
+  def read_config
+    YAML.load(File.open(CONFIG))
+  end
+
+  def poll
+    @config[:servers].each do |name, url|
+      uri = URI.parse(url)
+      Net::HTTP.start(uri.host, uri.port) do |http|
+        http.get(uri.path)
       end
     end
-    status_hash
-  end
-
-  def style_tag
-    ["<style>
-      body { font-family: \"Lucida Grande\", sans-serif; width: 640px; margin: 2em auto; background-color: #eeefff; }
-       h1 { margin-top: 0; }
-       #container {  text-align: center; padding: 2em; -moz-border-radius: 5px; border: 1px solid #666; background-color: white; }
-       .host { text-transform: uppercase; font-weight: bold; }
-      .up { color: green; }
-      .down { color: red; }
-      </style>"]
   end
 end
--- test/sup_test.rb
+++ test/sup_test.rb
@@ -1,6 +1,39 @@
+$:.push File.join(File.dirname(__FILE__), '..', 'lib')
 require 'test/unit'
+require 'rubygems'
+require 'shoulda'
+require 'mocha'
+require 'sup'
 
-def test_tests
-  assert true
+
+class SupTest < Test::Unit::TestCase
+  context "a new Sup instance" do
+    setup do
+      @sup = Sup.new
+    end
+
+    before_should "read server configs from a yml file" do
+      YAML.expects(:load)
+    end
+  end
+
+  context "when polling" do
+    setup do
+      @sup = Sup.new
+      @sup.poll
+    end
+
+    before_should "attempt to reach the server at the specified url" do
+      fake_config = {:servers =>
+        {:test1 => 'http://fake.server/index.html'}
+      }
+      Sup.any_instance.expects(:read_config).returns(fake_config)
+      Net::HTTP.expects(:start).yields(mock(:get))
+    end
+
+    should "display the HTTP status returned by the request" do
+
+    end
+  end
+
 end
-
